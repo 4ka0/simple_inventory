6ca92125898f21fe9702b5a82f4b59d8c616e965
Jon Lee <39420293+4ka0@users.noreply.github.com>
2021-04-30 15:23:38 +0900

    Misc. update

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("fish1234/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("home.urls")),
     path("stock/", include("stock.urls")),
